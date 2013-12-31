1bd2536ba2f7f58d4997ad87457d13a4071dfff7
Matthew Lewis <matt@mplewis.com>
2013-12-30 20:02:26 -0600

    update text

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var http = require('http');
 // Configure our HTTP server to respond with Hello World to all requests.
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'});
-  var text = 'Running Node.js:' + process.versions.node;
+  var text = 'Hello, Docker!\n This is Node version ' + process.versions.node + '.\n';
   response.end(text);
 });
 
